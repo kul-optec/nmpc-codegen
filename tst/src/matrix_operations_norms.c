09f3abb10c3e9624530d1b1e6d92e22a1fa46314
Willem Melis <zilleplus@gmail.com>
2017-08-31 11:31:02 +0200

    added test for the 2-norm macro

--- tst/src/matrix_operations_norms.c
+++ tst/src/matrix_operations_norms.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 
 int test2norm(void);
+int test2normMacro(void);
 
 int main(){
-    return test2norm();
+    return test2norm()+test2normMacro();
 }
 
 /* 
@@ -14,6 +15,21 @@ int main(){
 int test2norm(void){
     real_t testVector[4] = {1, 2, 2, 4};
     real_t testNorm = norm2_vector(testVector,4);
+    if(testNorm==5){ 
+        return 0;
+    }else{
+        printf("The 2 norm of [1; 2; 2; 4] should be 5 but is %f",testNorm);
+        return 1; /* test failed so return 1 */
+    }
+}
+
+/* 
+ * test the 2 norm macro: the 2 norm of [1 2 2 4] should be 5
+ */
+int test2normMacro(void){
+    real_t testVector[4] = {1, 2, 2, 4};
+    real_t testNorm;
+    VECTOR_2NORM(testVector,4,testNorm)
     if(testNorm==5){ 
         return 0;
     }else{
