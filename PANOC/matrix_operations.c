0e64b259a6421c9df1e07754b5351125c392ed70
Willem Melis <zilleplus@gmail.com>
2017-09-28 20:53:30 +0200

    added lots of consts

--- PANOC/matrix_operations.c
+++ PANOC/matrix_operations.c
@@ -7,7 +7,7 @@
  * calculate the 2 norm of a vector defined as
  * sqrt(x[0]^2 + x[1]^2 + ... x[n]^2)
  */
-real_t vector_norm2(real_t* vector,size_t vector_size){
+real_t vector_norm2(const real_t* vector,const size_t vector_size){
     real_t norm=0;
     size_t i;
     for(i=0;i<vector_size;i++){
@@ -17,46 +17,43 @@ real_t vector_norm2(real_t* vector,size_t vector_size){
 }
 
 /* copy vector1 into vector2 */
-void vector_copy(real_t* vector1,real_t* vector2,size_t size_vector){
+void vector_copy(const real_t* vector1,real_t* vector2,const size_t size_vector){
     size_t i;
     for(i=0; i < size_vector; i++)vector2[i]=vector1[i];
 }
 
 /* add vector1 and vector2 save the result in result */
-void vector_add(real_t* vector1,real_t* vector2,size_t size_vector,real_t* result){
+void vector_add(const real_t* vector1,const real_t* vector2,const size_t size_vector,real_t* result){
     size_t i;
     for(i = 0; i < size_vector; i++)result[i]=vector1[i]+vector2[i];
 }
 
 /* add vector with real */
-void vector_real_add(real_t* vector,size_t size_vector,real_t real,real_t* result){
+void vector_real_add(const real_t* vector,const size_t size_vector,const real_t real,real_t* result){
     size_t i;
     for(i = 0; i < size_vector; i++)result[i]=vector[i]+real;
 }
 
 /** subtract vector2 from vector1 save the result in result */
-void vector_sub(real_t* vector1,real_t* vector2,size_t size_vector,real_t* result){
+void vector_sub(const real_t* vector1,const real_t* vector2,const size_t size_vector,real_t* result){
     size_t i;
     for(i = 0; i < size_vector; i++)result[i]=vector1[i]-vector2[i];
 }
 
 /* multiply vector with real */
-void vector_real_mul(real_t* vector,size_t size_vector,real_t real,real_t* result){
+void vector_real_mul(const real_t* vector,const size_t size_vector,const real_t real,real_t* result){
     size_t i;
     for(i = 0; i < size_vector; i++)result[i]=vector[i]*real;
 }
 
 /*  multiply each element of vector1 times -1 and save it in vector2 */
-void vector_minus(real_t* vector1,real_t* vector2,size_t size_vector){
-    while(size_vector>0){
-        *vector2 = - *vector1;
-        vector1++; vector2++;
-        size_vector--;
-    }
+void vector_minus(const real_t* vector1,real_t* vector2,const size_t size_vector){
+    size_t i;
+    for(i = 0; i < size_vector; i++)vector2[i]=-vector1[i];
 }
 
 /* inner product between vector 1 and 2 */
-real_t inner_product(real_t* vector1,real_t* vector2,size_t size_vector){
+real_t inner_product(const real_t* vector1,const real_t* vector2,const size_t size_vector){
     size_t i;
     real_t result=0;
     for (i = 0; i < size_vector; i++)
@@ -67,7 +64,7 @@ real_t inner_product(real_t* vector1,real_t* vector2,size_t size_vector){
 }
 
 /* add vector2 n times from vector1 */
-void vector_add_ntimes(real_t* vector1,real_t* vector2,size_t size_vector,real_t n,real_t* result){
+void vector_add_ntimes(const real_t* vector1,const real_t* vector2,const size_t size_vector,const real_t n,real_t* result){
     size_t i;
     for(i = 0; i < size_vector; i++)result[i]=vector1[i]+n*vector2[i];
 }
--- PANOC/matrix_operations.h
+++ PANOC/matrix_operations.h
@@ -9,33 +9,33 @@
 
 
 /* copy vector1 into vector2 */
-void vector_copy(real_t* vector1,real_t* vector2,size_t size_vector);
+void vector_copy(const real_t* vector1,real_t* vector2,const size_t size_vector);
 
 /* add vector1 and vector2 save the result in result */
-void vector_add(real_t* vector1,real_t* vector2,size_t size_vector,real_t* result);
+void vector_add(const real_t* vector1,const real_t* vector2,const size_t size_vector,real_t* result);
 
 /* add vector with real */
-void vector_real_add(real_t* vector,size_t size_vector,real_t real,real_t* result);
+void vector_real_add(const real_t* vector,const size_t size_vector,const real_t real,real_t* result);
 
 /* subtract vector2 from vector1 save the result in result */
-void vector_sub(real_t* vector1,real_t* vector2,size_t size_vector,real_t* result);
+void vector_sub(const real_t* vector1,const real_t* vector2,const size_t size_vector,real_t* result);
 
 /* multiply vector with real */
-void vector_real_mul(real_t* vector,size_t size_vector,real_t real,real_t* result);
+void vector_real_mul(const real_t* vector,const size_t size_vector,const real_t real,real_t* result);
 
 /*
  * calculate the 2 norm of a vector defined as
  * sqrt(x[0]^2 + x[1]^2 + ... x[n]^2)
  */
-real_t vector_norm2(real_t* vector,size_t vector_size);
+real_t vector_norm2(const real_t* vector,const size_t vector_size);
 
 /*  multiply each element of vector1 times -1 and save it in vector2 */
-void vector_minus(real_t* vector1,real_t* vector2,size_t size_vector);
+void vector_minus(const real_t* vector1,real_t* vector2,const size_t size_vector);
 
 /* inner product between vector 1 and 2 */
-real_t inner_product(real_t* vector1,real_t* vector2,size_t size_vector);
+real_t inner_product(const real_t* vector1,const real_t* vector2,const size_t size_vector);
 
 /* add vector2 n times to vector1 */
-void vector_add_ntimes(real_t* vector1,real_t* vector2,size_t size_vector,real_t n,real_t* result);
+void vector_add_ntimes(const real_t* vector1,const real_t* vector2,const size_t size_vector,const real_t n,real_t* result);
 
 #endif
