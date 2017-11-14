7b27d705e7f222a54adbd9ac08eecc523e1e9cde
Willem Melis <zilleplus@gmail.com>
2017-11-14 21:18:30 +0100

    fixed minor bug in chain dynamic

--- tst/python/chainDyn_test.py
+++ tst/python/chainDyn_test.py
@@ -16,10 +16,11 @@ model_params = Chain_dyn_parameters(dimension,number_of_balls,ball_mass,
 spring_constant,rest_length_of_springs,gravity_acceleration)
 
 # initial state:
-x0 = np.array([ 1.,0.,2.,0.,3.,0.,4.,0.,5.,0.,6.,0., 0.,0.,0.,0.,0. ])
-x0 = x0.T
-
+x0 = np.array([ 1.,0.,2.,0.,3.,0.,4.,0.,5.,0.,6.,0., \
+                0.,0.,0.,0.,0.,0., 0.,0.,0.,0 ])
+x0 = x0.reshape([22,1])
 u0 = np.array([6,0])
+
 function_system = lambda x: chain_dyn(x, u0, model_params)
 # test = function_system(x0)
 step_size=0.1
@@ -28,5 +29,7 @@ f_discreet = lambda x: integrator_explicit_euler(x,step_size,function_system)
 current_state = f_discreet(x0)
 print(current_state)
 for i in range(1,10):
-    current_state = f_discreet(current_state)
-    print(current_state)
+    new_current_state = f_discreet(current_state)
+    current_state=new_current_state
+
+print(current_state)
