8931c4022dbff79535db7d5d36e2f92f0e35cbee
Willem Melis <zilleplus@gmail.com>
2017-12-05 13:06:02 +0100

    cleaned up the simulator

--- src_python/simulator.py
+++ src_python/simulator.py
@@ -8,46 +8,40 @@ class Simulator:
     """ simulator used to interact in python with an controller in c """
     def __init__(self,location):
         self._location=location
-        self.compile_interface()
-
-        if(platform.system()=='Linux'):
-            print("Compiling python interface for Linux")
-            extension_lib = '.so'
-            lib_location = location + "libpython_interface" + extension_lib
-            self.nmpc_python_interface = ctypes.CDLL(lib_location)
-        elif(platform.system()=='Windows'):
-            extension_lib = '.dll'
-            lib_location = location + "libpython_interface" + extension_lib
-            print("Compiling python interface for Windows: "+lib_location)
-            self.nmpc_python_interface = ctypes.windll.LoadLibrary(lib_location)
-        else:
-            print("ERROR platform can't be detected, using Linux")
-            extension_lib = '.so'
-
-        
-        # self.nmpc_python_interface.simulation_init.argtypes = ()
-        # self.nmpc_python_interface.simulate_nmpc_panoc.argtypes = (ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double))
-        # self.nmpc_python_interface.simulation_cleanup.argtypes = ()
-
-        self.simulator_init()
+    def load_library(self):
+        try:
+            if (platform.system() == 'Linux'):
+                print("Compiling python interface for Linux")
+                extension_lib = '.so'
+                lib_location = self._location + "libpython_interface" + extension_lib
+                self.nmpc_python_interface = ctypes.CDLL(lib_location)
+            elif (platform.system() == 'Windows'):
+                extension_lib = '.dll'
+                lib_location = self._location + "libpython_interface" + extension_lib
+                print("Compiling python interface for Windows: " + lib_location)
+                self.nmpc_python_interface = ctypes.windll.LoadLibrary(lib_location)
+            else:
+                print("ERROR platform can't be detected, using Linux")
+                extension_lib = '.so'
+        except:
+            print("Failed to load the dll, are you sure python and the toolchain are compatible?")
+            print("check if they both are either 32bit or both 64 bit")
     def simulate_nmpc(self,current_state,input_size):
-        # global nmpc_python_interface       
         length_state = len(current_state)
         array_state = ctypes.c_double * length_state
 
         array_optimal_input = ctypes.c_double * input_size
         optimal_input = array_optimal_input()
 
-        # result = self.nmpc_python_interface.simulate_nmpc_panoc(array_state(*current_state), array_optimal_input(*optimal_input))
-
         result = ctypes.c_double
         result = self.nmpc_python_interface.simulate_nmpc_panoc(\
             array_state(*current_state),\
             optimal_input\
             )
-        print(type(result))
         return (result, optimal_input)
     def simulator_init(self):
+        self.compile_interface()
+        self.load_library()
         self.nmpc_python_interface.simulation_init()
     def simulator_cleanup(self):
         self.nmpc_python_interface.simulation_cleanup()
@@ -55,7 +49,6 @@ class Simulator:
         cwd = os.getcwd()
         try:
             os.chdir(self._location)
-            os.system("make python_interface")
-            # subprocess.call("make python_interface", shell=False)
+            os.system("make clean python_interface")
         finally:
             os.chdir(cwd)
