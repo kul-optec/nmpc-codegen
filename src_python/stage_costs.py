e8f8ede75353e5abaac7383bd7258200ddb978f5
Willem Melis <zilleplus@gmail.com>
2017-12-21 10:16:57 +0100

    Fixed bug: first state was not used in stage cost

--- src_python/stage_costs.py
+++ src_python/stage_costs.py
@@ -8,11 +8,11 @@ class Stage_cost_QR:
         # As state and input are of the stype csadi.SX we can't just do vector matrix product
         # Everything must be written out in basic operations
         stage_cost=0
-        for i_col in range(1,self._model.number_of_states):
-            for i_row in range(1, self._model.number_of_states):
+        for i_col in range(0,self._model.number_of_states):
+            for i_row in range(0, self._model.number_of_states):
                 stage_cost += state[i_col]*self._Q[i_col,i_row]*state[i_row]
-        for i_col in range(1,self._model.number_of_inputs):
-            for i_row in range(1, self._model.number_of_inputs):
+        for i_col in range(0,self._model.number_of_inputs):
+            for i_row in range(0, self._model.number_of_inputs):
                 stage_cost += input[i_col]*self._R[i_col,i_row]*input[i_row]
         return stage_cost
 
@@ -27,12 +27,12 @@ class Stage_cost_QR_reference:
         # As state and input are of the stype csadi.SX we can't just do vector matrix product
         # Everything must be written out in basic operations
         stage_cost=0
-        for i_col in range(1,self._model.number_of_states):
-            for i_row in range(1, self._model.number_of_states):
+        for i_col in range(0,self._model.number_of_states):
+            for i_row in range(0, self._model.number_of_states):
                 stage_cost += (state[i_col]-self._state_reference[i_col])*\
                                self._Q[i_col,i_row]*\
                               (state[i_row]-self._state_reference[i_row])
-        for i_col in range(1,self._model.number_of_inputs):
-            for i_row in range(1, self._model.number_of_inputs):
+        for i_col in range(0,self._model.number_of_inputs):
+            for i_row in range(0, self._model.number_of_inputs):
                 stage_cost += input[i_col]*self._R[i_col,i_row]*input[i_row]
         return stage_cost
