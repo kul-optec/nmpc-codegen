821155a5e058c3a38acc770d2f5433329f1d7481
Willem Melis <zilleplus@gmail.com>
2018-01-15 16:42:49 +0100

    moved demo scripts together

--- demos/python/chain_dyn_casadi_demo.py
+++ demos/python/chain_dyn_casadi_demo.py
@@ -3,44 +3,35 @@
 
 import sys
 sys.path.insert(0, '../../src_python')
-from integrators import *
-import model_continious as modelc
-import models.chain_model as mc
+import nmpccodegen as nmpc
+import nmpccodegen.models as models
+import nmpccodegen.example_models as example_models
 
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-
-# model parameters(use the same as the Matlab code):
-dimension=2
-number_of_balls=4 # M
-ball_mass=0.03 # m
-spring_constant=0.1 # D
-rest_length_of_springs=0.033 # L
-gravity_acceleration=9.81
-
-model_params = mc.Chain_dyn_parameters(dimension,number_of_balls,ball_mass,
-spring_constant,rest_length_of_springs,gravity_acceleration)
 
 # initial state:
 initial_state = np.array([ 0.2, 0. , 0.4,0. , 0.6 , 0. , 0.8 , 0. \
                 ,1. , 0., \
                 0.,0., 0.,0.,0.,0.,0.,0. ])
 initial_state = initial_state.reshape([18,1])
-
 input = np.zeros([2,1]) # do not change the handle
 
 step_size=0.01
 simulation_time=5
 number_of_steps= math.ceil(simulation_time/step_size)
 
-system_equations = lambda state,input: mc.chain_dyn(state, input, model_params)
+# get the continuous system equations from the existing library
+(system_equations, number_of_states, number_of_inputs, coordinates_indices) = nmpc.get_chain_model()
+dimension=2
+number_of_balls = 4
+
 integrator="RK"
 g=0 # is not used here so just put it on zero
-model =modelc.Model_continious(system_equations,g,step_size,model_params.number_of_states,model_params.number_of_inputs,integrator)
+model =models.Model_continious(system_equations,g,step_size,number_of_states,number_of_inputs,coordinates_indices,integrator)
 
 current_state = model.get_next_state(initial_state,input)
-print(current_state)
 for i in range(1,number_of_steps):
     new_current_state =model.get_next_state(current_state,input)
     current_state=new_current_state
@@ -48,9 +39,3 @@ for i in range(1,number_of_steps):
 final_positions = np.concatenate(\
     (np.zeros((dimension,1)),np.reshape(current_state[0:dimension*(number_of_balls+1)],(number_of_balls+1,dimension)).T)\
 ,axis=1)
-
-print(current_state)
-print(final_positions)
-
-plt.plot(final_positions[0,:],final_positions[1,:])
-plt.show()
---
+++ demos/python/globals_generator_demo.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.insert(0, '../../src_python')
+import nmpccodegen as nmpc
+import nmpccodegen.tools as tools
+import nmpccodegen.models as models
+import nmpccodegen.controller as controller
+import nmpccodegen.Cfunctions as cfunctions
+
+import numpy as np
+
+def main():
+    (system_equations, number_of_states, number_of_inputs, coordinates_indices) = nmpc.example_models.get_trailer_model(
+        L=0.5)
+
+    test_generator = controller.Globals_generator("./test_globals.c")
+
+    step_size=0.1
+    integrator = "RK"
+    constraint_input = cfunctions.IndicatorBoxFunction([-1, -1], [1, 1])  # input needs stay within these borders
+    model = models.Model_continious(system_equations, constraint_input, step_size, number_of_states, \
+                                    number_of_inputs, coordinates_indices, integrator)
+
+    Q = np.diag([1., 100., 1.])
+    R = np.eye(model.number_of_inputs, model.number_of_inputs) * 1.
+
+    # reference_state=np.array([2,2,0])
+    stage_cost = controller.stage_costs.Stage_cost_QR(model, Q, R)
+
+    # define the controller, set the controller on whatever location, as we won't generate code it doesnt matter
+    trailer_controller = controller.Nmpc_panoc("./", model, stage_cost)
+
+    test_generator.generate_globals(trailer_controller)
+
+if __name__ == "__main__":
+    main()
