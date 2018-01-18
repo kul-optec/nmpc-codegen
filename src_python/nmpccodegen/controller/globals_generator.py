2c10fca0f487ed0a14b72ab0a9bb54119652e074
Willem Melis <zilleplus@gmail.com>
2018-01-18 17:57:15 +0100

    added docs to globals generator

--- src_python/nmpccodegen/controller/globals_generator.py
+++ src_python/nmpccodegen/controller/globals_generator.py
@@ -4,28 +4,30 @@ class Globals_generator:
     def __init__(self,location_globals):
         self._location_globals=location_globals
     def generate_globals(self,nmpc_controller):
+        """ generate globals header file at location defined by constructor """
         print("Generating globals file at: "+self._location_globals)
-        self.init_globals_file()
+        self._init_globals_file()
 
-        self.generate_title("Problem specific definitions")
-        self.define_variable("DIMENSION_INPUT", nmpc_controller.model.number_of_inputs)
-        self.define_variable("DIMENSION_STATE", nmpc_controller.model.number_of_states)
-        self.define_variable("MPC_HORIZON", nmpc_controller.horizon)
+        self._generate_title("Problem specific definitions")
+        self._define_variable("DIMENSION_INPUT", nmpc_controller.model.number_of_inputs)
+        self._define_variable("DIMENSION_STATE", nmpc_controller.model.number_of_states)
+        self._define_variable("MPC_HORIZON", nmpc_controller.horizon)
 
         self.set_data_type(nmpc_controller.data_type)
 
-        self.generate_title("lbgfs solver definitions")
-        self.define_variable("LBGFS_BUFFER_SIZE",nmpc_controller.lbgfs_buffer_size)
+        self._generate_title("lbgfs solver definitions")
+        self._define_variable("LBGFS_BUFFER_SIZE",nmpc_controller.lbgfs_buffer_size)
 
-        self.generate_title("NMPC-PANOC solver definitions")
-        self.define_variable("PANOC_MAX_STEPS",nmpc_controller.panoc_max_steps)
+        self._generate_title("NMPC-PANOC solver definitions")
+        self._define_variable("PANOC_MAX_STEPS",nmpc_controller.panoc_max_steps)
 
-        self.generate_title("Optional features")
+        self._generate_title("Optional features")
         if( nmpc_controller.integrator_casadi):
-            self.define_variable("INTEGRATOR_CASADI","1")
+            self._define_variable("INTEGRATOR_CASADI","1")
 
 
-    def init_globals_file(self):
+    def _init_globals_file(self):
+        """ internal function called at the start of the file generation """
         # replace globals file of it doesnt exist
         globals_file = open(self._location_globals, 'w')
 
@@ -34,7 +36,8 @@ class Globals_generator:
                            " at " + time.strftime("%H:%M:%S") + " */" + "\n\n")
 
         globals_file.close()
-    def define_variable(self,variable_name,variable_value):
+    def _define_variable(self,variable_name,variable_value):
+        """ internal function generate a preprocessor variable """
         globals_file = open(self._location_globals, 'a')
 
         lines = ["#define ",variable_name ," ",str(variable_value),"\n"]
@@ -42,12 +45,14 @@ class Globals_generator:
 
         globals_file.close()
     def generate_comment(self,comment):
+        """ internal function generate single comment line """
         globals_file = open(self._location_globals, 'a')
 
         globals_file.write("/* "+comment+" */ \n")
 
         globals_file.close()
-    def generate_title(self,title):
+    def _generate_title(self,title):
+        """ internal function used to generate titles in comments """
         globals_file = open(self._location_globals, 'a')
 
         globals_file.write("/*"+"\n" + "* ---------------------------------"+"\n")
@@ -56,23 +61,24 @@ class Globals_generator:
 
         globals_file.close()
     def set_data_type(self,data_type):
+        """ set the datatype to either "single precision" or "double precision" """
         if(data_type=="single precision"):
-            self.generate_title("constants used with float data type")
-            self.define_variable("real_t","float")
+            self._generate_title("constants used with float data type")
+            self._define_variable("real_t","float")
             self.generate_comment("data types have different absolute value functions")
-            self.define_variable("ABS(x)", "fabsf(x)")
+            self._define_variable("ABS(x)", "fabsf(x)")
             self.generate_comment("Machine accuracy of IEEE float")
-            self.define_variable("MACHINE_ACCURACY", "pow(10,-8)")
+            self._define_variable("MACHINE_ACCURACY", "pow(10,-8)")
             self.generate_comment("large number use with things like indicator functions")
-            self.define_variable("LARGE", "100000")
+            self._define_variable("LARGE", "100000")
         elif(data_type=="double precision"):
-            self.generate_title("constants used with double data type")
-            self.define_variable("real_t", "double")
+            self._generate_title("constants used with double data type")
+            self._define_variable("real_t", "double")
             self.generate_comment("data types have different absolute value functions")
-            self.define_variable("ABS(x)", "fabs(x)")
+            self._define_variable("ABS(x)", "fabs(x)")
             self.generate_comment("Machine accuracy of IEEE float")
-            self.define_variable("MACHINE_ACCURACY", "pow(10,-16)")
+            self._define_variable("MACHINE_ACCURACY", "pow(10,-16)")
             self.generate_comment("large number use with things like indicator functions")
-            self.define_variable("LARGE", "10000000000")
+            self._define_variable("LARGE", "10000000000")
         else:
             print("Error: invalid data type, not supported by globals generator")
