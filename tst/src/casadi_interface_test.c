b697619606e344c7d86a2d62b0bd39340c90e065
Willem Melis <zilleplus@gmail.com>
2018-01-29 20:55:38 +0100

    fixed casadi_interface tests, no idea how this ended up this bad

--- tst/src/casadi_interface_test.c
+++ tst/src/casadi_interface_test.c
@@ -18,10 +18,15 @@ static real_t input_reference[20]={ 0,0,0,0,0,
                                     0,0,0,0,0,
                                     0,0,0,0,0};
 
+static void print_success(void){
+    printf("## TEST SUCCEEDED ## \n");
+}
+
 int main(){
-    return test_casadi_interface_f()+\
-        test_casadi_interface_df()+\
-        simple_test_integrator();
+    int test_result1 = test_casadi_interface_f();
+    int test_result2 = test_casadi_interface_df();
+        // simple_test_integrator();
+    return test_result1+test_result2;
 }
 
 int test_casadi_interface_f(void){
@@ -29,10 +34,7 @@ int test_casadi_interface_f(void){
     printf("Starting test on casadi interface using the function in /casadi/cost_function.c \n");
     casadi_interface_init();
 
-    real_t zero_input[20]={0,0,0,0,0,
-                      0,0,0,0,0,
-                      0,0,0,0,0,
-                      0,0,0,0,0};
+    real_t* zero_input=calloc(sizeof(real_t),casadi_interface_get_dimension());
 
     real_t current_state[DIMENSION_STATE]={0.1932, -5.9190 , 0.3874,-8.8949,0.6126,-8.8949,0.8068,-5.9190 \
                 ,1. , 0., \
@@ -42,28 +44,36 @@ int test_casadi_interface_f(void){
     real_t test_cost_optimal = casadi_interface_f(zero_input);
 
     real_t lower_state[DIMENSION_STATE]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    vector_add_ntimes(lower_state,current_state,18,0.8,lower_state);
+    vector_add_ntimes(lower_state,current_state,DIMENSION_STATE,0.8,lower_state);
     
-    casadi_prepare_cost_function(current_state,state_reference,input_reference);
+    casadi_prepare_cost_function(lower_state,state_reference,input_reference);
     real_t test_cost_to_low = casadi_interface_f(zero_input);
 
     real_t higher_state[DIMENSION_STATE]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    vector_add_ntimes(higher_state,current_state,18,1.5,higher_state);
+    vector_add_ntimes(higher_state,current_state,DIMENSION_STATE,1.5,higher_state);
     
-    casadi_prepare_cost_function(current_state,state_reference,input_reference);
+    casadi_prepare_cost_function(higher_state,state_reference,input_reference);
     real_t test_cost_to_high = casadi_interface_f(zero_input);
 
-    printf("test of f");
+    printf("test of f \n");
     printf("The 3 costs are: %f - %f - %f\n",test_cost_to_low,test_cost_optimal,test_cost_to_high); 
-    if(test_cost_to_low<test_cost_optimal) return FAILURE;
-    if(test_cost_to_high<test_cost_optimal) return FAILURE;
+    if(test_cost_to_low<test_cost_optimal) {
+        printf("ERROR: lower state should give higher cost! \n");
+        return FAILURE;
+    }
+    if(test_cost_to_high<test_cost_optimal) {
+        printf("ERROR: higher state should give higher cost! \n");
+        return FAILURE;
+    }
     
+    free(zero_input);
     casadi_interface_cleanup();
+    print_success();
     return SUCCESS; 
 }
 int test_casadi_interface_df(void){
     printf("-----------------\n");
-    printf("Starting test on casadi interface using the function in /casadi/cost_function.c \n");
+    printf("Starting test on casadi interface using the function in /casadi/cost_function_derivative_combined.c \n");
     casadi_interface_init();
 
     real_t current_state[DIMENSION_STATE]={0.1932, -5.9190 , 0.3874,-8.8949,0.6126,-8.8949,0.8068,-5.9190 \
@@ -71,47 +81,49 @@ int test_casadi_interface_df(void){
                 0.,0., 0.,0.,0.,0.,0.,0.};
 
     /* test on current state */
-    real_t zero_input[20]={0,0,0,0,0,
-                      0,0,0,0,0,
-                      0,0,0,0,0,
-                      0,0,0,0,0};
+    real_t* zero_input=calloc(sizeof(real_t),casadi_interface_get_dimension());
                       
     casadi_prepare_cost_function(current_state,state_reference,input_reference);
-    real_t test_cost_optimal[2];
+    real_t* test_cost_optimal=malloc(sizeof(real_t)*casadi_interface_get_dimension());
     casadi_interface_f_df(zero_input,test_cost_optimal);
     /* --------------------- */
 
     /* test on lower current state */
-    real_t lower_state[DIMENSION_STATE]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    real_t* lower_state=calloc(sizeof(real_t),DIMENSION_STATE);
     vector_add_ntimes(lower_state,current_state,18,0.8,lower_state);
     
-    
     casadi_prepare_cost_function(current_state,state_reference,input_reference);
-    real_t test_cost_to_low[2];
+    real_t* test_cost_to_low=malloc(sizeof(real_t)*casadi_interface_get_dimension());
     casadi_interface_f_df(zero_input,test_cost_to_low);
+
+    free(lower_state);
     /* --------------------- */
 
     /* test on higher current state */
-    real_t higher_state[DIMENSION_STATE]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    real_t* higher_state=calloc(sizeof(real_t),DIMENSION_STATE);
     vector_add_ntimes(higher_state,current_state,18,1.5,higher_state);
         
     casadi_prepare_cost_function(current_state,state_reference,input_reference);
-    real_t test_cost_to_high[2];
+    real_t* test_cost_to_high=malloc(sizeof(real_t)*casadi_interface_get_dimension());
     casadi_interface_f_df(zero_input,test_cost_to_high);
+    free(higher_state);
     /* --------------------- */
 
     printf("{%f %f} \n",test_cost_to_low[0],test_cost_to_low[1]);
     printf("{%f %f} \n",test_cost_optimal[0],test_cost_optimal[1]);
     printf("{%f %f} \n",test_cost_to_high[0],test_cost_to_high[1]);
     
-    
+    free(zero_input);
+    free(test_cost_optimal);
+    free(test_cost_to_low);free(test_cost_to_high);
     casadi_interface_cleanup();
+    print_success();
     return SUCCESS; 
 }
 
 int simple_test_integrator(void){
     printf("-----------------\n");
-    printf("Starting test on npmc at  \n");
+    printf("Starting test on integrator at  \n");
     casadi_interface_init();
 
     real_t input[2]={0,0};
@@ -140,6 +152,7 @@ int simple_test_integrator(void){
     free(current_state);
     free(new_state);
     casadi_interface_cleanup();
+    if(return_value==SUCCESS) print_success();
     return return_value; 
 }
 
