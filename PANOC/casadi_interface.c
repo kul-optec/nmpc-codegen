5f8117134f92bb66575b6e884925f43da7f34d47
Willem Melis <zilleplus@gmail.com>
2018-02-01 15:15:54 +0100

    remove construct of unused arrays in casadi

--- PANOC/casadi_interface.c
+++ PANOC/casadi_interface.c
@@ -29,15 +29,9 @@ static CasadiFunction* integrator_data;
 #endif
 
 int casadi_interface_init(){
-    cost_function_data=init_buffer_casadi_function(cost_function,cost_function_work);
-    if(cost_function_data==NULL) goto fail_1;
-
-    cost_function_derivative_combined_data=init_buffer_casadi_function(cost_function_derivative_combined,cost_function_derivative_combined_work);
-    if(cost_function_derivative_combined_data==NULL) goto fail_2;
-
     if(NUMBER_OF_OBSTACLES>0){
         obstacle_weights=malloc(sizeof(real_t)*NUMBER_OF_OBSTACLES);
-        if(obstacle_weights==NULL) goto fail_3;
+        if(obstacle_weights==NULL) goto fail_1;
         else{
             size_t i;
             for (i = 0; i < NUMBER_OF_OBSTACLES; i++)
@@ -46,32 +40,13 @@ int casadi_interface_init(){
             }
         }
     }
-    
-
-    #ifdef INTEGRATOR_CASADI
-        integrator_data=init_buffer_casadi_function(integrator,integrator_work);
-        if(integrator_data==NULL) goto fail_4;
-    #endif
-
     return SUCCESS;
 
-    #ifdef INTEGRATOR_CASADI
-    fail_4:
-        if(NUMBER_OF_OBSTACLES>0) free(obstacle_weights);
-    #endif
-    fail_3:
-        cleanup_buffer_casadi_function(cost_function_derivative_combined_data);
-    fail_2:
-        cleanup_buffer_casadi_function(cost_function_data);
     fail_1:
         return FAILURE;
 }
 int casadi_interface_cleanup(){
-    cleanup_buffer_casadi_function(cost_function_data);
-    cleanup_buffer_casadi_function(cost_function_derivative_combined_data);
-    #ifdef INTEGRATOR_CASADI
-    cleanup_buffer_casadi_function(integrator_data);
-    #endif
+    free(obstacle_weights);
     return SUCCESS;
 }
 #ifdef INTEGRATOR_CASADI
@@ -79,12 +54,14 @@ int casadi_integrate(const real_t* current_state,const real_t* input,real_t* new
     const real_t* input_casadi[2]={current_state,input};
     real_t* output_casadi[1]={new_state};
 
+    return integrator(input_casadi, output_casadi, NULL, NULL, MEM_CASADI);
+
+    /* old code, the arrays are unused this way
     integrator_data->cost_function(input_casadi,output_casadi,\
         integrator_data->buffer_int,\
         integrator_data->buffer_real,\
         MEM_CASADI);
-
-    return SUCCESS;
+    */
 }
 #endif
 static const real_t* state;
@@ -109,11 +86,14 @@ real_t casadi_interface_f(const real_t* input){
 
     const real_t* input_function[5]={state,input,state_reference,input_reference,obstacle_weights};
 
+    cost_function(input_function, output, NULL, NULL, MEM_CASADI);
+
+    /* old code, the arrays are unused this way
     cost_function_data->cost_function(input_function,output,\
         cost_function_data->buffer_int,\
         cost_function_data->buffer_real,\
         MEM_CASADI);
-
+    */
     return *output[0];
 }
 
@@ -122,10 +102,14 @@ real_t casadi_interface_f_df(const real_t* input,real_t* data_output){
     real_t* output[2] = {&f_value,data_output};
     const real_t* input_function[5]={state,input,state_reference,input_reference,obstacle_weights};
 
+    cost_function_derivative_combined(input_function, output, NULL, NULL, MEM_CASADI);
+
+    /* old code, the arrays are unused this way
     cost_function_derivative_combined_data->cost_function(input_function,output,\
         cost_function_derivative_combined_data->buffer_int,\
         cost_function_derivative_combined_data->buffer_real,\
         MEM_CASADI);
+    */
     return f_value;
 }
 
