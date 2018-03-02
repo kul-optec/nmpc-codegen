d3d71d6e1a69ba7b948de47f2604a729af319f73
Willem Melis <zilleplus@gmail.com>
2018-03-02 12:31:57 +0100

    added example to generate controller in Matlab, no simulation possible yet

---
+++ demos/Matlab/TwoCircTrailer.m
@@ -0,0 +1,38 @@
+step_size=0.03;
+
+% Q and R matrixes determined by the control engineer.
+Q = diag([1. 1. 0.0])*0.2;
+R = diag([1. 1.]) * 0.01;
+
+Q_terminal = diag([1. 1. 0.0])*2;
+R_terminal = diag([1. 1.]) * 0.01;
+
+trailer_controller = prepare_demo_trailer(step_size,Q,R,Q_terminal,R_terminal);
+
+trailer_controller.horizon = 50; % NMPC parameter
+trailer_controller.integrator_casadi = true; % optional  feature that can generate the integrating used  in the cost function
+trailer_controller.panoc_max_steps = 500; % the maximum amount of iterations the PANOC algorithm is allowed to do.
+trailer_controller.min_residual=-3;
+
+% construct left circle
+left_circle = nmpccodegen.controller.obstacles.Obstacle_circular([0.2 0.2],0.2);
+
+% construct right circle
+right_circle = nmpccodegen.controller.obstacles.Obstacle_circular([0.7 0.2], 0.2);
+
+% add obstacles to controller
+trailer_controller.add_obstacle(left_circle);
+trailer_controller.add_obstacle(right_circle);
+
+% generate the dynamic code
+trailer_controller.generate_code();
+
+% simulate everything
+initial_state = [0.2 0.6 0];
+reference_state = [0.7 -0.02 pi/2];
+reference_input = [0 0];
+
+obstacle_weights = [1000. 1000.];
+
+% state_history = simulate_demo(trailer_controller,initial_state,...
+%     reference_state,reference_input,obstacle_weights);
---
+++ demos/Matlab/prepare_demo_trailer.m
@@ -0,0 +1,24 @@
+function trailer_controller = prepare_demo_trailer( step_size,Q,R,Q_terminal,R_terminal )
+%PREPARE_DEMO_TRAILER Prepare a controller 
+%   Construct the controller from the trailer model and bootstrap the
+%   enviroment. (output location is repo/test_controller_builds/demo_controller_matlab)
+
+    % generate static files
+    trailer_controller_output_location =  '../../test_controller_builds/demo_controller_matlab';
+    nmpccodegen.tools.Bootstrapper.bootstrap(trailer_controller_output_location, true);
+
+    % get example model from lib
+    [system_equations, number_of_states, number_of_inputs, coordinates_indices] ...
+        = nmpccodegen.example_models.get_trailer_model(0.5);
+
+    integrator = 'RK44';  % select a Runga-Kutta  integrator (FE is forward euler)
+    constraint_input = nmpccodegen.Cfunctions.IndicatorBoxFunction([-4, -4], [4, 4]);  % input needs stay within these borders
+    model = nmpccodegen.models.Model_continuous(system_equations, constraint_input, step_size, number_of_states, ...
+                                    number_of_inputs, coordinates_indices, integrator);
+
+    % define the controller
+    stage_cost = nmpccodegen.controller.Stage_cost_QR(model, Q, R);
+    terminal_cost = nmpccodegen.controller.Stage_cost_QR(model, Q_terminal, R_terminal);
+    trailer_controller = nmpccodegen.controller.Nmpc_panoc(trailer_controller_output_location, model, stage_cost,terminal_cost);
+end
+
