d1c56e03a80d9ff5d33a59a0511c0a1818bfd346
Willem Melis <zilleplus@gmail.com>
2018-03-19 15:31:05 +0100

    seperated square opration from softconstraint evaluation.

--- src_matlab/+nmpccodegen/+controller/+obstacles/Obstacle.m
+++ src_matlab/+nmpccodegen/+controller/+obstacles/Obstacle.m
@@ -1,6 +1,8 @@
 classdef Obstacle < matlab.mixin.Heterogeneous
-    %OBSTACLE Summary of this class goes here
-    %   Detailed explanation goes here
+    %OBSTACLE Abstract class that represents the minimum interface of an
+    %obstacle
+    %   An obstacle needs at least the following interface:
+    %       - evaluate_cost , evaluates the cost of the soft constraint
     
     properties
     end
@@ -9,8 +11,8 @@ classdef Obstacle < matlab.mixin.Heterogeneous
         evaluate_cost(obj,coordinates_state);
     end
     methods(Static)
-        function tqs_x = trim_and_square(x)
-            tqs_x = max(x,0)^2;
+        function tqs_x = trim(x)
+            tqs_x = max(x,0);
         end
     end
 end
