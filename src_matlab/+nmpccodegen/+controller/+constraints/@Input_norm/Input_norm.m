c9dfe1af804982814facb50b6827f9740f5d01af
Willem Melis <zilleplus@gmail.com>
2018-03-28 17:27:57 +0200

    squaring of the norm gives better results

--- src_matlab/+nmpccodegen/+controller/+constraints/@Input_norm/Input_norm.m
+++ src_matlab/+nmpccodegen/+controller/+constraints/@Input_norm/Input_norm.m
@@ -12,7 +12,7 @@ classdef Input_norm < nmpccodegen.controller.constraints.Constraint
         end
         function cost = evaluate_cost(obj,state,input)
             norm_input = sum1(input.^2);
-            cost = max(norm_input - obj.max_norm^2,0); % if norm is higer then max_norm return penalty
+            cost = max(norm_input - obj.max_norm^2,0)^2; % if norm is higer then max_norm return penalty
         end        
     end
     
