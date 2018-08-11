c22e9c7e52827af7627eb945bafdcd30de9d02d3
Willem Melis <willem.melis@student.kuleuven.be>
2018-08-11 13:23:48 +0200

    added doc strings to python tools

--- src_python/nmpccodegen/tools/bootstrapper.py
+++ src_python/nmpccodegen/tools/bootstrapper.py
@@ -7,10 +7,23 @@ import sys
 
 
 class Bootstrapper:
-    """ bootstraps an nmpc environment """
+    """ 
+    Bootstraps an nmpc environment 
+    """
     def bootstrap(output_location_controller,simulation_tools=False):
         location_nmpc_repo = Bootstrapper._get_repo_location()
-        """ bootstrap the nmpc at location nmpc """
+        """ 
+        Create a folder that contains all the static files needed to
+             create a controller.
+
+        Parameters
+        ---------
+        output_location_controller : Output location of the folder.
+        simulation_tools : (default=falses )Should be set on true if you want
+        to use the simulator. If set on false, the bootstrapper
+        will only provide the absolute minimum files to create a
+        controller. 
+        """
         print("GENERATING output folders of controller:")
         Bootstrapper._bootstrap_folders(output_location_controller)
 
@@ -27,7 +40,9 @@ class Bootstrapper:
 
     @staticmethod
     def _get_repo_location():
-        """ finds out where the lib is installed """
+        """ 
+        Finds out where the lib is installed 
+        """
         bootstrapper_location = os.path.dirname(os.path.realpath(__file__))
         repo_location = os.path.dirname(os.path.dirname(os.path.dirname(bootstrapper_location)))
 
@@ -35,7 +50,13 @@ class Bootstrapper:
 
     @staticmethod
     def _bootstrap_folders(lib_location):
-        """ bootstrap the nmpc at location nmpc """
+        """ 
+        Bootstrap the nmpc at location nmpc 
+
+        Parameters
+        ---------
+        lib_location : output location of bootstrapper
+        """
         Bootstrapper._create_folder_if_not_exist(lib_location)
         Bootstrapper._create_folder_if_not_exist(lib_location+"/casadi")
         Bootstrapper._create_folder_if_not_exist(lib_location + "/globals")
@@ -45,6 +66,15 @@ class Bootstrapper:
 
     @staticmethod
     def _generate_PANOC_lib(location,location_nmpc_repo,overwrite):
+        """
+        Copy .c/.h files of panoc algorithm
+
+        Parameters
+        ---------
+        location : output location
+        location_nmpc_repo : location nmpccodegen library
+        overwrite : if true, overwrite existing files
+        """
         src_files = ["buffer.c","buffer.h","casadi_definitions.h","lbfgs.h","lbfgs.c","lipschitz.c","lipschitz.h",\
                      "matrix_operations.h","matrix_operations.c","nmpc.c","panoc.c","panoc.h",\
                      "proximal_gradient_descent.c","proximal_gradient_descent.h","casadi_interface.c","casadi_interface.h"]
@@ -60,12 +90,30 @@ class Bootstrapper:
 
     @staticmethod
     def _generate_static_globals(location,location_nmpc_repo,overwrite):
+        """
+        Copy static header file of globals
+
+        Parameters
+        ---------
+        location : output location
+        location_nmpc_repo : location nmpccodegen library
+        overwrite : if true, overwrite existing files
+        """
         src_location = location_nmpc_repo + "/globals/" + "globals.h"
         dst_location = location + "/globals/" + "globals.h"
         Bootstrapper._copy_over_file(src_location,dst_location,overwrite)
 
     @staticmethod
     def _generate_python_interface(location, location_nmpc_repo, overwrite):
+        """
+        Copy .c/.h files of the Python/Matlab interface
+
+        Parameters
+        ---------
+        location : output location
+        location_nmpc_repo : location nmpccodegen library
+        overwrite : if true, overwrite existing files
+        """
         src_files = ["nmpc_python.c","nmpc_python.h","timer.h","timer_linux.c","timer_windows.c","timer_mac.c"]
 
         for i in range(0, len(src_files)):
@@ -75,6 +123,15 @@ class Bootstrapper:
 
     @staticmethod
     def _generate_build_system(location,location_nmpc_repo,overwrite):
+        """
+        Copy over Cmake build system files
+
+        Parameters
+        ---------
+        location : output location
+        location_nmpc_repo : location nmpccodegen library
+        overwrite : if true, overwrite existing files
+        """
         src_location = location_nmpc_repo + "/minimum_build_system/" + "CMakeLists_root.txt"
         dst_location = location + "/CMakeLists.txt"
         Bootstrapper._copy_over_file(src_location,dst_location,overwrite)
