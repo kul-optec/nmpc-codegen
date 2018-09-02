3b3c7b1282cd8c649c38416cd6b091d4b8e64ea7
mkrishan <manish.krishanlal@esat.kuleuven.be>
2018-09-02 18:34:27 +0200

    replaced stack with heap for one_norm

--- C_proximal_functions/src/one_norm.c
+++ C_proximal_functions/src/one_norm.c
@@ -8,6 +8,7 @@
 
 struct one_norm* init_one_norm(const unsigned int dimension,const real_t mu){
     struct one_norm* data = malloc(sizeof(struct one_norm));
+    
     if(data==NULL) return NULL;
 
     data->dimension=dimension;
@@ -19,21 +20,27 @@ struct one_norm* init_one_norm(const unsigned int dimension,const real_t mu){
 static real_t sign_x(real_t x) { return x<0 ? -1 : x>0 ? 1 : x;}
 
 
-struct L1_norm* prox_one_norm(const struct one_norm* data,real_t* input, real_t gamma){
+real_t  prox_one_norm(const struct one_norm* data,real_t* input, real_t gamma){
     /* prox=sign(x)*max{|x_i|-\lambda,0} */
     unsigned int i;
+
+    int *ptr, sum;
+    ptr=(real_t*)malloc(data->dimension*sizeof(real_t));
+
     lamda    = data->mu * gamma;
     real_t g = 0.*lamda; 
-
+    sum=0; //assign 0 to replace garbage value
     for (i = 0; i < data->dimension; i++){
-        intermediate[i] = fmax(0.0, abs(input[i]) - lamda);
-        prox[i]   = sign_x(input[i])*intermediate[i];
+        input[i] = sign_x(input)*fmax(0.0, abs(input[i]) - lamda);
+        sum+=*(ptr+input[i]); 
     }
-    struct output { 
-        prox;
-        g_prox = data->mu*SUM(intermediate, data->dimension, sum);  
-    }
-    /* return strcut containging proximal point= x_{bar} and  for g(x_{bar}) = mu * ||x_{bar}||_{1} */
-    return output;
+    
+    g = data->mu*sum; 
+    free(ptr);
+    return g;
 }
+	
+
+
+
 
